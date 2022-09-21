3da59ea222356a33ed492492b7d671928853c2d0
AiDinaAgustin <95268109+AiDinaAgustin@users.noreply.github.com>
2022-09-21 11:00:20 +0700

    Update Tugas1.java

--- Sesi1/Tugas1.java
+++ Sesi1/Tugas1.java
@@ -1,1 +1,12 @@
-
+public class TugasSatu {
+    public static void main(String[] args) {
+        /* Buatlah program JAVA yang berfungsi untuk mencetak output di layar monitor seperti berikut ini: 
+           “Belajar JAVA” 
+           “Sangat mudah sekali” 
+           “dan sangat menyenangkan” 
+        */
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
